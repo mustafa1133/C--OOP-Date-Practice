422c149034bdf97efd0f2dd833dbe170d71f0c76
mustafa1133 <mustafa1133@hotmail.com>
2019-04-26 16:47:19 -0400

    first commit

---
+++ LAB3/Date.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3
+{
+    class Date
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+
+        public Date(int ayear, int amonth, int aday)
+        {
+            Year = ayear;
+            Month = amonth;
+            Day = aday;
+        }
+
+        public string TellAboutSelf()
+        {
+            return $"The year is is: {Year}\n The Month is: {Month}\n The Day is: {Day}";
+        }
+
+        public void AddDays(int howMany)
+        {
+            int total = Day + howMany;
+            Year = Year+ total / 365;
+            Month =(total % 365) / 30;
+            Day = (total % 365) % 30;
+            }
+        }
+    }
---
+++ LAB3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date aDate = new Date(2019, 9, 20);
+            aDate.AddDays(1000000);
+            Console.WriteLine(aDate.TellAboutSelf()); 
+        }
+    }
+}
